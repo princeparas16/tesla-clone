b7d718593be3f33d28206a5df1ca246cfabe4d5c
princeparas16 <princeparas16@gmail.com>
2021-02-05 02:21:44 +0530

    header and assets added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Inkman</h1>
-      <h1>Inkman</h1>
+      <Header />
     </div>
   );
 }
--- src/hello.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const hello = () => {
-  return <div>whatsapp</div>;
-};
-
-export default hello;
